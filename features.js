e4bcac480afbf3cafcf1c9efb90371258aaf13a2
Iffat74 <iffat.zerin01@northsouth.edu>
2024-09-25 23:22:20 +0600

    history error fixed

---
+++ features.js
@@ -0,0 +1,17 @@
+
+document.getElementById('donation-btn').addEventListener('click', function(){
+    showSectionById('donation-form')
+    this.style.backgroundColor='limegreen'
+    document.getElementById('history-btn').style.backgroundColor='white'
+    
+})
+
+
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    showSectionById('history-display')
+ 
+    this.style.backgroundColor='limegreen'
+    document.getElementById('donation-btn').style.backgroundColor='white'
+  
+}) 
